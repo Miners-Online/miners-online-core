fb3e09b997188169200377188812abcba0bab3e7
ajh123 <ajh123@users.noreply.github.com>
2024-01-04 18:09:41 +0000

    fix: fix jar names and velocity plugin

--- core/velocity/src/main/java/uk/minersonline/core/velocity/MinersOnlineCore.java
+++ core/velocity/src/main/java/uk/minersonline/core/velocity/MinersOnlineCore.java
@@ -1,11 +1,13 @@
 package uk.minersonline.core.velocity;
 
+import com.google.inject.Inject;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.proxy.ProxyShutdownEvent;
 import com.velocitypowered.api.command.CommandManager;
 import com.velocitypowered.api.command.CommandMeta;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.player.ServerConnectedEvent;
+import com.velocitypowered.api.plugin.Dependency;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.ProxyServer;
@@ -22,13 +24,17 @@ import uk.minersonline.core.velocity.command.LobbyCommand;
 		version = BuildConstants.VERSION,
 		description = "The core plugin for the Miners Online server",
 		url = "https://minersonline.uk",
-		authors = {"ajh123"}
+		authors = {"ajh123"},
+		dependencies = {
+				@Dependency(id = "subservers-sync")
+		}
 )
 public class MinersOnlineCore {
 	private SubAPI subAPI;
     private final ProxyServer proxy;
 	private final Logger logger;
 
+	@Inject
     public MinersOnlineCore(ProxyServer proxy, Logger logger) {
         this.proxy = proxy;
 		this.logger = logger;
