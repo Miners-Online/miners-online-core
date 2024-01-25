3b69a25e38e61c11c976dda9817203e2f1def5f5
ajh123 <samrosoft36@gmail.com>
2024-01-25 15:51:15 +0000

    feat: add modses to command

--- minestom/lobby/src/main/java/uk/minersonline/minestom/lobby/stuff/MoreCats.java
+++ minestom/lobby/src/main/java/uk/minersonline/minestom/lobby/stuff/MoreCats.java
@@ -1,11 +1,10 @@
 package uk.minersonline.minestom.lobby.stuff;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.command.CommandManager;
-import net.minestom.server.command.CommandSender;
 import net.minestom.server.command.builder.Command;
-import net.minestom.server.command.builder.CommandContext;
 import net.minestom.server.command.builder.arguments.ArgumentType;
 import net.minestom.server.command.builder.condition.Conditions;
 import net.minestom.server.entity.EntityCreature;
@@ -34,50 +33,105 @@ public class MoreCats extends Command {
 	 */
 	private final static Map<UUID, Boolean> isRunning = new HashMap<>();
 
+	public enum CatCommandMode {
+		ASK("ask"),
+		SET("set"),
+		RESET("reset"),
+		SUMMON("summon");
+
+		private final String name;
+
+		CatCommandMode(String name) {
+			this.name = name;
+		}
+
+		@Override
+		public String toString() {
+			return this.name;
+		}
+	}
+
 	public MoreCats() {
 		super("cats");
 
 		// Make this command can only be run by players.
 		setCondition(Conditions::playerOnly);
 		// If no arguments are provided we want default code to run.
-		setDefaultExecutor(this::execute);
-		// Create an integer argument.
+		setDefaultExecutor((sender, context) -> {
+			String commandName = context.getCommandName();
+			sender.sendMessage(Component.text("Usage: /" + commandName + " <ASK|RESET|SET> [cat_count]", NamedTextColor.RED));
+		});
+
+		// Create an enum argument
+		var commandNode = ArgumentType.Enum("mode", CatCommandMode.class);
+		addSyntax((sender, context) -> {
+			switch (context.get(commandNode)) {
+				case ASK -> {
+					if (sender instanceof Player player) {
+						player.sendMessage("How many cats do you want? Please chat \"yes\" multiple times for the number of cats you want.");
+						isRunning.put(player.getUuid(), true);
+						if (!catCount.containsKey(player.getUuid())) {
+							catCount.put(player.getUuid(), 0);
+						}
+					}
+				}
+				case SET -> {
+					String commandName = context.getCommandName();
+					sender.sendMessage(Component.text("Required parameter `cat_count` is missing, Usage: /" + commandName + " SET [cat_count]", NamedTextColor.RED));
+				}
+				case RESET -> {
+					if (sender instanceof Player player) {
+						player.sendMessage("Reset your cat count, you now have 0.");
+						catCount.put(player.getUuid(), 0);
+					}
+				}
+				case SUMMON -> {
+					if (sender instanceof Player player) {
+						if (!catCount.containsKey(player.getUuid())) {
+							sender.sendMessage(Component.text("You need more then 0 cats.", NamedTextColor.RED));
+						} else {
+							int curCatCount = catCount.get(player.getUuid());
+							isRunning.put(player.getUuid(), false);
+							player.sendMessage("Summoned " + curCatCount + " cat(s)");
+							summonCats(player, curCatCount);
+							player.sendMessage("Reset your cat count, you now have 0.");
+							catCount.put(player.getUuid(), 0);
+						}
+					}
+				}
+			}
+		}, commandNode);
+
+		commandNode.setCallback((sender, exception) -> {
+			final String input = exception.getInput();
+			sender.sendMessage(Component.text("The mode " + input + " is invalid!", NamedTextColor.RED));
+			sender.sendMessage(Component.text("Please choose `ASK`, `SET`, or `RESET`.", NamedTextColor.RED));
+		});
+
+		// Create an integer argument
 		var newCats = ArgumentType.Integer("cat_count");
 		// Add code to be run when an integer is provided.
 		addSyntax((sender, context) -> {
-			// Get the integer from the user.
-			final int newCatCount = context.get(newCats);
-			// Check if a player has run the command.
-			if (sender instanceof Player player) {
-				// Check if the player does not exist
-				if (!catCount.containsKey(player.getUuid())) {
-					// If so, set the count to 0.
-					catCount.put(player.getUuid(), 0);
+			if (context.get(commandNode) == CatCommandMode.SET) {
+				// Get the integer from the user.
+				final int newCatCount = context.get(newCats);
+				// Check if a player has run the command.
+				if (sender instanceof Player player) {
+					// Append the new count to the current count.
+					catCount.put(player.getUuid(), newCatCount);
+					// Create a message telling the user the new cats.
+					player.sendMessage("You now have " + newCatCount + " cat(s)");
 				}
-				// Get the current cat count.
-				int curCatCount = catCount.get(player.getUuid());
-	 			// Append the new count to the current count.
-				catCount.put(player.getUuid(), curCatCount + newCatCount);
-				// Create a message telling the user the new cats.
-				player.sendMessage("You now have "+(curCatCount+newCatCount)+" cat(s)");
-				summonCats(player, (curCatCount+newCatCount));
+			} else {
+				sender.sendMessage(Component.text("The mode " + context.get(commandNode) + " is invalid!", NamedTextColor.RED));
+				sender.sendMessage(Component.text("You must use `SET`.", NamedTextColor.RED));
 			}
-		}, newCats);
+		}, commandNode, newCats);
 
 		newCats.setCallback((sender, exception) -> {
 			final String input = exception.getInput();
-			sender.sendMessage("The number " + input + " is invalid!");
+			sender.sendMessage(Component.text("The number " + input + " is invalid!", NamedTextColor.RED));
 		});
-	}
-
-	private void execute(CommandSender sender, CommandContext context) {
-		if (sender instanceof Player player) {
-			player.sendMessage("How many cats do you want? Please chat \"yes\" multiple times for the number of cats you want.");
-			isRunning.put(player.getUuid(), true);
-			if (!catCount.containsKey(player.getUuid())) {
-				catCount.put(player.getUuid(), 0);
-			}
-		}
 	}
 
 	/**
@@ -116,7 +170,6 @@ public class MoreCats extends Command {
 					int curCatCount = catCount.get(event.getPlayer().getUuid());
 					isRunning.put(event.getPlayer().getUuid(), false);
 					event.getPlayer().sendMessage("You now have "+curCatCount+" cat(s)");
-					summonCats(event.getPlayer(), curCatCount);
 				}
 			}
 		});
