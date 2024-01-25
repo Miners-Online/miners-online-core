d9cf49856c3a36587933123c5a239061b5bd4c33
ajh123 <samrosoft36@gmail.com>
2024-01-25 10:52:04 +0000

    fix: fix cats command

--- minestom/lobby/src/main/java/uk/minersonline/minestom/lobby/stuff/MoreCats.java
+++ minestom/lobby/src/main/java/uk/minersonline/minestom/lobby/stuff/MoreCats.java
@@ -5,13 +5,11 @@ import net.minestom.server.command.CommandManager;
 import net.minestom.server.command.CommandSender;
 import net.minestom.server.command.builder.Command;
 import net.minestom.server.command.builder.CommandContext;
+import net.minestom.server.command.builder.arguments.ArgumentType;
 import net.minestom.server.command.builder.condition.Conditions;
 import net.minestom.server.entity.Player;
 import net.minestom.server.event.GlobalEventHandler;
 import net.minestom.server.event.player.PlayerChatEvent;
-import net.minestom.server.event.player.PlayerSpawnEvent;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.*;
 
@@ -36,14 +34,45 @@ public class MoreCats extends Command {
 	public MoreCats() {
 		super("cats");
 
+		// Make this command can only be run by players.
 		setCondition(Conditions::playerOnly);
+		// If no arguments are provided we want default code to run.
 		setDefaultExecutor(this::execute);
+		// Create an integer argument.
+		var newCats = ArgumentType.Integer("cat_count");
+		// Add code to be run when an integer is provided.
+		addSyntax((sender, context) -> {
+			// Get the integer from the user.
+			final int newCatCount = context.get(newCats);
+			// Check if a player has run the command.
+			if (sender instanceof Player player) {
+				// Check if the player does not exist
+				if (!catCount.containsKey(player.getUuid())) {
+					// If so, set the count to 0.
+					catCount.put(player.getUuid(), 0);
+				}
+				// Get the current cat count.
+				int curCatCount = catCount.get(player.getUuid());
+				// Append the new count to the current count.
+				catCount.put(player.getUuid(), curCatCount + newCatCount);
+				// Create a message telling the user the new cats.
+				player.sendMessage("You now have "+(curCatCount+newCatCount)+" cat(s)");
+			}
+		}, newCats);
+
+		newCats.setCallback((sender, exception) -> {
+			final String input = exception.getInput();
+			sender.sendMessage("The number " + input + " is invalid!");
+		});
 	}
 
 	private void execute(CommandSender sender, CommandContext context) {
 		if (sender instanceof Player player) {
 			player.sendMessage("How many cats do you want? Please chat \"yes\" multiple times for the number of cats you want.");
 			isRunning.put(player.getUuid(), true);
+			if (!catCount.containsKey(player.getUuid())) {
+				catCount.put(player.getUuid(), 0);
+			}
 		}
 	}
 
